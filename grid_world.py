02adb9957018cfc956932f9577e8977ca9021dbf
ddhruvkr <ddhruvkr@gmail.com>
2017-07-09 03:28:31 +0530

    adding code for iterativePolicy and correcting gridWorld

--- grid_world.py
+++ grid_world.py
@@ -17,9 +17,9 @@ class Grid:
 		# (x,y) --> list of actions which are allowed
 		self.actions = actions
 
-	def setState(self, x, y):
-		self.x = x
-		self.y = y
+	def setState(self, s):
+		self.x = s[0]
+		self.y = s[1]
 
 	def getCurrentState(self):
 		return (self.x, self.y)
@@ -31,7 +31,10 @@ class Grid:
 
 	def allStates(self):
 		#states would either have an action or a rewards associated with it
-		return set(self.actions.keys() + self.rewards.keys())
+		states = set()
+		states.update(self.actions.keys())
+		states.update(self.rewards.keys())
+		return states
 
 	def isGameOver(self):
 		#since a terminal state would have no action
@@ -40,66 +43,69 @@ class Grid:
 	def move(self, action):
 		if action in self.actions[(self.x, self.y)]:
 			if action == 'U':
-				self.y += 1
-			elif action == 'D':
-				self.y -= 1
-			elif action == 'L':
 				self.x -= 1
-			elif action == 'R':
+			elif action == 'D':
 				self.x += 1
-		return self.rewards.get((self.x, self.y), 0)
+			elif action == 'L':
+				self.y -= 1
+			elif action == 'R':
+				self.y += 1
+		if (self.x, self.y) in self.rewards:
+			return self.rewards.get((self.x, self.y))
+		else:
+			return 0
 
 	def undoMove(self, action):
 		if action == 'U':
-			self.y -= 1
-		elif action == 'D':
-			self.y += 1
-		elif action == 'L':
 			self.x += 1
-		elif action == 'R':
+		elif action == 'D':
 			self.x -= 1
+		elif action == 'L':
+			self.y += 1
+		elif action == 'R':
+			self.y -= 1
 		assert(self.getCurrentState() in self.allStates())
 
-	def standardGrid():
-		# define a grid that describes the reward for arriving at each state
-		# and possible actions at each state
-		# the grid looks like this
-		# x means you can't go there
-		# s means start position
-		# number means reward at that state
-		# .  .  .  1
-		# .  x  . -1
-		# s  .  .  .
-		g = Grid(3, 4, (2, 0))
-		rewards = {(0, 3): 1, (1, 3): -1}
-		actions = {
-			(0, 0): ('D', 'R'),
-			(0, 1): ('L', 'R'),
-			(0, 2): ('L', 'D', 'R'),
-			(1, 0): ('U', 'D'),
-			(1, 2): ('U', 'D', 'R'),
-			(2, 0): ('U', 'R'),
-			(2, 1): ('L', 'R'),
-			(2, 2): ('L', 'R', 'U'),
-			(2, 3): ('L', 'U'),
-			}
-		g.set(rewards, actions)
-		return g
+def standardGrid():
+	# define a grid that describes the reward for arriving at each state
+	# and possible actions at each state
+	# the grid looks like this
+	# x means you can't go there
+	# s means start position
+	# number means reward at that state
+	# .  .  .  1
+	# .  x  . -1
+	# s  .  .  .
+	g = Grid(3, 4, (2, 0))
+	rewards = {(0, 3): 1, (1, 3): -1}
+	actions = {
+		(0, 0): ('D', 'R'),
+		(0, 1): ('L', 'R'),
+		(0, 2): ('L', 'D', 'R'),
+		(1, 0): ('U', 'D'),
+		(1, 2): ('U', 'D', 'R'),
+		(2, 0): ('U', 'R'),
+		(2, 1): ('L', 'R'),
+		(2, 2): ('L', 'R', 'U'),
+		(2, 3): ('L', 'U'),
+		}
+	g.setRewardsActions(rewards, actions)
+	return g
 
 
-	def negativeGrid(stepCost=-0.1):
-		# in this game we want to try to minimize the number of moves
-		# so we will penalize every move
-		g = standard_grid()
-		g.rewards.update({
-			(0, 0): step_cost,
-			(0, 1): step_cost,
-			(0, 2): step_cost,
-			(1, 0): step_cost,
-			(1, 2): step_cost,
-			(2, 0): step_cost,
-			(2, 1): step_cost,
-			(2, 2): step_cost,
-			(2, 3): step_cost,
-			})
-		return g
+def negativeGrid(stepCost=-0.1):
+	# in this game we want to try to minimize the number of moves
+	# so we will penalize every move
+	g = standard_grid()
+	g.rewards.update({
+		(0, 0): step_cost,
+		(0, 1): step_cost,
+		(0, 2): step_cost,
+		(1, 0): step_cost,
+		(1, 2): step_cost,
+		(2, 0): step_cost,
+		(2, 1): step_cost,
+		(2, 2): step_cost,
+		(2, 3): step_cost,
+		})
+	return g
---
+++ iterative_policy_evaluation.py
@@ -0,0 +1,108 @@
+import numpy as np
+from grid_world import standardGrid
+
+smallEnough = 10e-4
+def print_values(V, g):
+  for i in range(g.width):
+    print ("---------------------------")
+    for j in range(g.height):
+      v = V.get((i,j), 0)
+      if v >= 0:
+        print (" %.2f|" % v, end="")
+      else:
+        print ("%.2f|" % v, end="") # -ve sign takes up an extra space
+    print ("")
+
+
+def print_policy(P, g):
+  for i in range(g.width):
+    print ("---------------------------")
+    for j in range(g.height):
+      a = P.get((i,j), ' ')
+      print ("  %s  |" % a, end="")
+    print ("")
+
+if __name__ == '__main__':
+
+	print('Hola')
+	grid = standardGrid()
+	states = grid.allStates()
+
+	valueF = {}
+	for s in states:
+		valueF[s] = 0
+
+	while True:
+		gamma = 1
+		largest = 0
+		for s in states:
+			if s in grid.actions:
+				newValue = 0
+				oldStateValue = valueF[s]
+				pA = 1/len(grid.actions.get(s))
+				#print (pA)
+				
+				for a in grid.actions[s]:
+					grid.setState(s)
+					# movees and returns a reward which we get on moving
+					r = grid.move(a)
+					#print(s)
+					#print(a)
+					#print (grid.getCurrentState())
+					newValue += pA * (r + gamma*(valueF[grid.getCurrentState()]))
+				valueF[s] = newValue
+				largest = max(largest, np.abs(valueF[s] - oldStateValue))
+
+		if largest < smallEnough:
+			break
+
+	print ("values for uniformly random actions:")
+	print_values(valueF, grid)
+	print ("\n\n")
+
+
+	### fixed policy ###
+	policy = {
+	(2, 0): 'U',
+	(1, 0): 'U',
+	(0, 0): 'R',
+	(0, 1): 'R',
+	(0, 2): 'R',
+	(1, 2): 'R',
+	(2, 1): 'R',
+	(2, 2): 'R',
+	(2, 3): 'U',
+	}
+	print_policy(policy, grid)
+				
+	valueF = {}
+	for s in states:
+		valueF[s] = 0
+
+	while True:
+		gamma = 0.9
+		largest = 0
+		for s in states:
+			if s in policy:
+				newValue = 0
+				a = policy[s]
+				oldStateValue = valueF[s]
+				pA = 1
+				#print (pA)
+				
+				grid.setState(s)
+				# movees and returns a reward which we get on moving
+				r = grid.move(a)
+				#print(s)
+				#print(a)
+				#print (grid.getCurrentState())
+				newValue += pA * (r + gamma*(valueF[grid.getCurrentState()]))
+				valueF[s] = newValue
+				largest = max(largest, np.abs(valueF[s] - oldStateValue))
+
+		if largest < smallEnough:
+			break
+
+	print ("values for uniformly random actions:")
+	print_values(valueF, grid)
+	print ("\n\n")
